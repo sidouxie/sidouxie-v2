dbd9d571b7b41b3f4d6bc3775dcb32f482717f50
sidouxie <sid_ouxiee@hotmail.com>
2021-06-17 16:14:27 +0100

    static dirname

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+//Middleware
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
